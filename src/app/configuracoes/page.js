e8bd0afeae6f3225343afaa7c95b59024f9dd260
Mariana <mariana.souza2@ufu.br>
2025-12-11 01:01:43 -0300

    Atualização da página de configurações

--- src/app/configuracoes/page.js
+++ src/app/configuracoes/page.js
@@ -1,48 +1,49 @@
-import Image from "next/image";
-import Link from 'next/link';
-import { Inter } from "next/font/google";
+'use client';
+
 import './configuracoes.css'; 
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
 const inter = Inter({
     subsets: ["latin"],
-    weight: ["400", "600", "800"],
+    weight: ["800"],
 });
 
 export default function Configuracoes() {
     return (
-        <div className={`pagina-scroll ${inter.className}`}>
+        <div className="cfg-page">
             
-            <div className="header-topo">
-                <Image
-                    src="/Group.svg" 
-                    alt="Ícone Configurações"
-                    width={40}  
-                    height={40} 
-                />
-                <h1>Configurações</h1>
+            <div className="cfg-header-container">
+                <div className="cfg-titulo-header">
+                    <Image src="/Group.svg"
+                     alt="Ícone Configurações"
+                      width={70} 
+                      height={70} /> 
+                    <h1 className={inter.className}>Configurações</h1>
+                </div>
             </div>
 
-            <div className="card-padrao">
-                
-                <h2 className="titulo-secao">Logotipo da ótica</h2>
-                
-                <div className="linha-conteudo">
-                    <div className="box-upload">
-                        <p className="texto-upload">
-                            Clique para<br/>
-                            adicionar<br/>
-                            uma imagem
-                        </p>
+            
+            <div className="cfg-form-container">
+                <h2>Logotipo da ótica</h2>
+
+                <div className="cfg-logo-section">
+                    <div className="cfg-box-upload">
+                        Clique para adicionar<br/>uma imagem
                     </div>
 
-                    <div className="grupo-input">
+                    <div className="cfg-input-wrapper">
                         <label>Nome</label>
-                        <input type="text" />
+                        <input type="text" className="cfg-input-padrao" />
                     </div>
+
                 </div>
+            </div>
 
-                
-
+        
+            <div className="cfg-botoes-container">
+                <button className="cfg-btn-acao cfg-btn-salvar">Salvar</button>
+                <button className="cfg-btn-acao cfg-btn-cancelar">Cancelar</button>
             </div>
 
         </div>
